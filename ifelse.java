ed8324322223aedcdf3c45f74ced7d265d0407d3
suresh patel <61912237+suresh1047@users.noreply.github.com>
2021-11-26 14:37:43 +0530

    Create ifelse.java
    find largest number from 2 numbers.

---
+++ ifelse.java
@@ -0,0 +1,15 @@
+package ifelse;
+import java.util.Scanner;
+public class GreaterNumberTwo {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter 1st number: ");
+		int num1 = sc.nextInt();
+		System.out.print("Enter 2nd number: ");
+		int num2 = sc.nextInt();
+		if(num1 > num2) {
+			System.out.println(num1 +" is greater.");
+		}else
+			System.out.println(num2 +" is greater.");
+	}
+}
