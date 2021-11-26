a62a9ff7a7efdc4a7ca8e494ef1f03da6c8cd7a6
suresh patel <61912237+suresh1047@users.noreply.github.com>
2021-11-26 15:00:04 +0530

    Create nestedifelse.java

---
+++ nestedifelse.java
@@ -0,0 +1,24 @@
+package ifelse;
+import java.util.Scanner;
+public class NestedIfElse {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter 1st number: ");
+		int num1 = sc.nextInt();
+		System.out.print("Enter 2nd number: ");
+		int num2 = sc.nextInt();
+		System.out.print("Enter 3rd number: ");
+		int num3 = sc.nextInt();
+		if(num1 > num2) {
+			if (num1 > num3) {
+				System.out.println(num1 +" is largest number.");
+			}else
+				System.out.println(num3 +" is largest number.");
+		}else
+		if (num2 > num3) {
+			System.out.println(num2 +" is largest number.");
+			}else
+				System.out.println(num3 +" is largest number.");
+	}
+}
