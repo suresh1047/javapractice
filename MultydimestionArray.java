7a44313dd39afea017fc7104f71b5b37e58aa80d
suresh patel <61912237+suresh1047@users.noreply.github.com>
2021-11-26 20:25:58 +0530

    Create MultydimestionArray.java

---
+++ MultydimestionArray.java
@@ -0,0 +1,43 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class MultydimestionArray {
+
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter the row of multydimestion array: ");
+	int r = sc.nextInt();
+	System.out.println("Enter the column of multydimestion array: ");
+	int c = sc.nextInt();
+	int arr1[][] = new int [r][c];
+	int arr2[][] = new int [r][c];
+	System.out.println("Enter multydimestion array 1st: ");
+	for(int i = 0; i < r; i++) {
+		for(int j = 0; j < c; j++) {
+			arr1[i][j] = sc.nextInt();
+		}
+	}
+	System.out.println("Enter multydimestion array 2st: ");
+	for(int i = 0; i < r; i++) {
+		for(int j = 0; j < c; j++) {
+			arr2[i][j] = sc.nextInt();
+		}
+	}
+	int arrsum[][] = new int [r][c];
+	for(int i = 0; i < r; i++) {
+		for(int j = 0; j < c; j++) {
+			arrsum[i][j] = arr1[i][j] + arr2[i][j];
+		}
+		
+	}
+	System.out.println("Sum of multydimestion array 1st and 2nd: ");
+	for(int i = 0; i < r; i++) {
+		for(int j = 0; j < c; j++) {
+			System.out.print(arrsum[i][j] + " ");
+		}
+		System.out.println();
+	}
+	}
+
+}
